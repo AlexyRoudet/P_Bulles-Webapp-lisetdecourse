f45701318dd35935c23c2514a7890243c8bd27a8
marPages13 <122774913+marPages13@users.noreply.github.com>
2023-11-27 10:16:01 +0100

    Upload s'une version avec le port par défaut pour effectuer des tests

---
+++ webapp-listedecourse3/ressources/php/get_element.php
@@ -0,0 +1,40 @@
+<?php
+// Configuration de la connexion à la base de données MySQL
+$servername = "127.0.0.1";
+$username = "root";
+$password = ".etml-";
+$dbname = "db_listedecourse";
+
+try {
+    // Créez une connexion PDO à la base de données MySQL
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+    // Préparer la requête SQL
+    $stmt = $conn->prepare("SELECT eleElement FROM t_element");
+
+    // Exécuter la requête SQL
+    $stmt->execute();
+    
+    // Récupérer les résultats de la requête SQL sous forme de tableau associatif
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+    // Afficher les résultats
+    http_response_code(200);
+    foreach ($results as $row) {
+        echo $row['eleElement'] . '<br>';
+    }
+
+    // Fermez la connexion à la base de données MySQL
+    $conn = null;
+
+    //redirige vers la page d'origine
+    header('Location: ' . '../../index.php');
+    exit;
+}
+catch(PDOException $e) 
+{
+  http_response_code(500);
+  echo "Erreur lors de l'accès : " . $e->getMessage();
+}
+
+?>
